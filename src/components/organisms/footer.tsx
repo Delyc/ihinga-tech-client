e8288235b545de1a62b001b30699bdf99c4083e6
delyc <delyce35@gmail.com>
2025-04-27 12:10:59 +0200

    feat: name

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer className="bg-primary-green dark:bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 py-12">
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-32">
           <div>
             <h2 className="text-lg font-semibold mb-2">Umurage Farm</h2>
             <p className="text-sm">
@@ -17,14 +17,15 @@ export const Footer = () => {
 
 
 
-          {/* <div>
-            <h3 className="text-md font-semibold mb-2">Legal</h3>
+          <div>
+            <h3 className="text-md font-semibold mb-2">Address</h3>
             <ul className="space-y-1">
-              <li><a href="/privacy" className="hover:underline">Privacy Policy</a></li>
-              <li><a href="/terms" className="hover:underline">Terms of Service</a></li>
-              <li><a href="/cookies" className="hover:underline">Cookie Policy</a></li>
+              <li><a href="/privacy" className="hover:underline">District: Rwamagana </a></li>
+              <li><a href="/terms" className="hover:underline">Sector: Nyakaliro</a></li>
+              <li><a href="/cookies" className="hover:underline">Cell: Munini</a></li>
+              <li><a href="/cookies" className="hover:underline">Village: Akamasatura</a></li>
             </ul>
-          </div> */}
+          </div>
         </div>
         <Separator className="my-8 border-white opacity-20" />
         <div className="flex flex-col md:flex-row justify-between items-center">
